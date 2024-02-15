2001053e1883ea2ce30e81e7cca3b31bb8b23933
manggury <148739312+manggury@users.noreply.github.com>
2024-02-15 16:36:29 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -7,11 +7,21 @@ $(function () {
             $('.header').removeClass("on");
         }
     }
-    $('.visual_wrap').slick({
-        arrows: true,
-        // fade: true,
-        autoplay: true,
+
+    $('.visual_wrap .inner').on('init afterChange', function () {
+        var currnet = $('.visual_wrap .inner .slick-current');
+        currnet.addClass("on").siblings().removeClass("on");
     });
+
+
+    $('.visual_wrap .inner').slick({
+        arrows: true,
+        fade: true,
+        autoplay: true,
+        pauseOnHover: false,
+    });
+
+
 
     $('.header .xi-bars').on('click', function (e) {
         e.preventDefault();
@@ -22,30 +32,30 @@ $(function () {
             e.preventDefault();
     });
 
-    document.addEventListener("DOMContentLoaded", function () {
-        var videoContainer = document.getElementById("videoContainer");
-        var videoOverlay = document.getElementById("videoOverlay");
-        var video = document.getElementById("scrollVideo");
 
-        // 비디오가 표시될 위치 계산
-        var videoTop = videoContainer.offsetTop;
-        var videoHeight = videoContainer.offsetHeight;
+    var videoContainer = document.getElementById("videoContainer");
+    var videoOverlay = document.getElementById("videoOverlay");
+    var video = document.getElementById("scrollVideo");
 
-        // 스크롤 이벤트 리스너 등록
-        window.addEventListener("scroll", function () {
-            // 스크롤 위치 계산
-            var scrollPosition = window.scrollY;
+    // 비디오가 표시될 위치 계산
+    var videoTop = videoContainer.offsetTop;
+    var videoHeight = videoContainer.offsetHeight;
 
-            // 특정 지점에 도달하면 비디오 재생
-            if (scrollPosition >= videoTop - window.innerHeight && scrollPosition <= videoTop + videoHeight) {
-                video.play();
-                videoOverlay.style.display = "none"; // 오버레이 숨김
-            } else {
-                video.pause();
-                videoOverlay.style.display = "flex"; // 오버레이 표시
-            }
-        });
+    // 스크롤 이벤트 리스너 등록
+    window.addEventListener("scroll", function () {
+        // 스크롤 위치 계산
+        var scrollPosition = window.scrollY;
 
+        // 특정 지점에 도달하면 비디오 재생
+        if (scrollPosition >= videoTop - window.innerHeight && scrollPosition <= videoTop + videoHeight) {
+            video.play();
+            videoOverlay.style.display = "none"; // 오버레이 숨김
+        } else {
+            video.pause();
+            videoOverlay.style.display = "flex"; // 오버레이 표시
+        }
     });
+
+
 
 });
